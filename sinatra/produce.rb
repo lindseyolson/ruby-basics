b667571cf54e30d4471b454d5d9b67fc995ea7ba
Lindsey Olson <lindseyolson00@gmail.com>
2017-09-08 18:12:58 -0500

    basic app setup and running

---
+++ sinatra/produce.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get("/apple") do
+  "<h1>Here's a juicy apple!<h1>"
+end
+
+get("/banana")do
+  "<h1>Here's a ripe banana!<h1>"
+end
+
+get("/carrot")do
+  "<h1>Here's a cruncy carrot!</h1>"
+end
