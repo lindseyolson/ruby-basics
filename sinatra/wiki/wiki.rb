b9a720f4daa19652b8a7622c5f5af7210c2f2f67
Lindsey Olson <lindseyolson00@gmail.com>
2017-09-11 16:50:47 -0500

    http requests with sinatra

--- sinatra/wiki/wiki.rb
+++ sinatra/wiki/wiki.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'uri'
 
 def page_content(title)
   File.read("pages/#{title}.txt")
@@ -6,10 +7,47 @@ rescue Errno::ENOENT
   return nil
 end
 
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
+def delete_content(title)
+  File.delete("pages/#{title}.txt")
+end
+
 get '/' do
   erb :welcome
 end
 
+get '/new' do
+  erb :new
+end
+
 get '/:title' do
-  page_content(params[:title])
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :show
+end
+
+get '/:title/edit' do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :edit
+end
+
+post '/create' do
+  save_content(params["title"], params["content"])
+  redirect URI.escape("/#{params["title"]}")
+end
+
+put '/:title' do
+  save_content(params["title"], params["content"])
+  redirect URI.escape("/#{params["title"]}")
+end
+
+delete '/:title' do
+  delete_content(params[:title])
+  redirect "/"
 end
